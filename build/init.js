0045e4194cd7e358a4a672bb742d8f143761b991
youredith <youredith3@gmail.com>
2023-05-09 20:22:33 +0900

    feat: hamburger menu

--- build/init.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import app from "./server.js";
-
-const PORT = process.env.PORT || 4000;
-
-const handleListening = () => console.log(`✅Server Listening on port ${PORT}🎉`);
-
-app.listen(PORT, handleListening);
--- build/routers/rootRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const rootRouter = express.Router();
-import { home } from "../controllers/globalControllers.js";
-
-rootRouter.get("/", home);
-
-export default rootRouter;
